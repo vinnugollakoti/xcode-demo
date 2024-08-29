cc7c363a377697eba2500858ad9cb900715bbd11
vinnugollakoti <vinnugollakoti289@gmail.com>
2024-08-29 08:17:30 +0530

    Initial Commit

---
+++ CPP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CPP
+//
+//  Created by Vinnu Gollakoti on 29/08/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
